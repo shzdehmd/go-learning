e288897934e936586306c49805bdcf92fe21c667
Ahmad Shahzad <107808273+shzdehmd@users.noreply.github.com>
2025-04-07 13:24:00 +0500

    assigning values to variables with Go short assignment.

---
+++ 02-variables/short-assignment.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt";
+
+func main() {
+
+	// Declaring a variable named "message" that can hold characters and words: string
+	// Assigning a value to the message variable using Go short assignment
+	message := "Learning Go variables!";
+
+	// Printing the value of the message variable
+	fmt.Println(message);
+
+	// Declaring a variable named "number" that can hold whole numbers: int
+	// Assigning a value to the number variable using Go short assignment
+	number := 10;
+
+	// Printing the value of the number variable
+	fmt.Println(number);
+
+	// Declaring another variable named "number2" that can hold whole numbers: int
+	// Assigning a value to the number2 variable using Go short assignment
+	number2 := 20;
+
+	// Printing the value of the number2 variable
+	fmt.Println(number2);
+}
