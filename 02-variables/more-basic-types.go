91e8eaf17b890d71b37a1300324d89aac4d0ab2a
Ahmad Shahzad <107808273+shzdehmd@users.noreply.github.com>
2025-04-07 13:24:24 +0500

    familiarizing myself with more basic types like floats and bools.

---
+++ 02-variables/more-basic-types.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt";
+
+func main() {
+	// -- MORE BASIC TYPES --
+
+	// Declaring a variable that can hold numbers with decimal points - floating point variables: float64
+	// Declaring with Go short assignment
+	price := 99.99;
+
+	// Printing the value of the price variable
+	fmt.Println(price);
+
+	// Declaring a variable that can hold true or false values - boolean variables: bool
+	// Declaring with Go short assignment
+	isGoFun := true;
+
+	// Printing the value of the isGoFun variable
+	fmt.Println("Is Go fun?", isGoFun);
+}
