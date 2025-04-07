f0cfdcca32268e823fa32a4cc966b3d949e9507a
Ahmad Shahzad <107808273+shzdehmd@users.noreply.github.com>
2025-04-07 13:25:00 +0500

    using fmt.Println with multiple variables and custom text.

---
+++ 02-variables/advanced-println.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt";
+
+func main() {
+	// Create a variable to store name and age
+	name := "Jake Daniels";
+	age := 65;
+
+	// Print the name and age with a message using fmt.Println
+	fmt.Println("Hello, my name is", name, "and I am", age, "years old.");
+}
